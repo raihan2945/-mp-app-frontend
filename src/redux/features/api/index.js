6257d10d34107464ccdde465f686f1575c1ba9f7
000raihan <000raihanahmad@gmail.com>
2024-06-06 17:43:45 +0600

    updated

--- src/redux/features/api/index.js
+++ src/redux/features/api/index.js
@@ -2,9 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedOut } from "../auth/authSlice";
 
 // const bUrl = "http://172.16.16.126:6070"
-const bUrl = "http://146.190.88.171:6005";
+// const bUrl = "http://146.190.88.171:6005";
 // const bUrl = "http://localhost:6005";
-// const bUrl = "http://116.68.200.97:6049";
+const bUrl = "http://116.68.200.97:56005";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: bUrl,
