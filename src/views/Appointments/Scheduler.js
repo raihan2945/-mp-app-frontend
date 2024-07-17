e0b31770f3316e51cbae135a4bfa8e2b0bd119f1
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-17 20:21:55 +0600

    feat: sync fetch data with calender according to month

--- src/views/Appointments/Scheduler.js
+++ src/views/Appointments/Scheduler.js
@@ -1,21 +1,38 @@
 import { useSearchParams } from "react-router-dom";
 import { weekdays } from "../../utils/data";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useGetAllAppointmentQuery } from "../../redux/features/appointment/appointmentApi";
+import dayjs from "dayjs";
+import { dateFormatter } from "../../utils/format";
 
 const Scheduler = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  let now = new Date(searchParams.get("m")) || new Date();
+
+  let now = searchParams.has("m")
+    ? new Date(searchParams.get("m"))
+    : new Date();
+
+    // fetch data
+  const { data, isLoading } = useGetAllAppointmentQuery({
+    start: dayjs(new Date(now.getFullYear(), now.getMonth(), 0)).format(
+      "YYYY-MM-DD",
+    ),
+    end: dayjs(new Date(now.getFullYear(), now.getMonth() + 1, 0)).format(
+      "YYYY-MM-DD",
+    ),
+  });
+
+
+  
+
+  // calender date setup
   const totalDays = new Date(
     now.getFullYear(),
     now.getMonth() + 1,
     0,
   ).getDate();
-
+  
   let dayList = [];
-
-  useEffect(() => {
-    now = new Date(searchParams.get("m"));
-  }, [searchParams]);
 
   for (let i = 1; i <= totalDays; i++) {
     dayList.push(new Date(`${now.getFullYear()}-${now.getMonth() + 1}-${i}`));
@@ -27,6 +44,8 @@ const Scheduler = () => {
       dayList.unshift("");
     }
   }
+
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <>
@@ -43,7 +62,21 @@ const Scheduler = () => {
           {dayList.map((item, idx) =>
             item != "" ? (
               <div className="calender-cell" key={idx}>
-                {item.getDate()}
+                <div className="date">{item.getDate()}</div>
+                <div className="events">
+                  {data &&
+                    data.data
+                      .filter((event) => {
+                        return (
+                          dateFormatter(event.start) == dateFormatter(item)
+                        );
+                      })
+                      .map((data) => (
+                        <div className="event" key={data.id}>
+                          {data.full_name}
+                        </div>
+                      ))}
+                </div>
               </div>
             ) : (
               <div className="calender-cell disabled" key={idx}></div>
