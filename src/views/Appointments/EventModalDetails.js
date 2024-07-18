786a3bd1bacaa714b0db71c0f30aab26ed3df160
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-07-18 17:47:44 +0600

    feat: modification in event detials modal

--- src/views/Appointments/EventModalDetails.js
+++ src/views/Appointments/EventModalDetails.js
@@ -1,9 +1,7 @@
 import React from "react";
 import dayjs from "dayjs";
 import { GoDotFill } from "react-icons/go";
-import { FaRegClock } from "react-icons/fa";
-
-
+import { FaRegClock, FaRegBuilding } from "react-icons/fa";
 
 const EventModalDetails = ({ eventData }) => {
   return (
@@ -12,22 +10,49 @@ const EventModalDetails = ({ eventData }) => {
         <div className="icon square"></div>
         <div className="cell">
           <h4>{eventData.full_name}</h4>
-        </div>
-      </div>
-
-
-      <div className="modal-cells">
-        <div><FaRegClock size={18} /></div>
-        <div className="cell">
           <h5>
-            {dayjs(eventData.start).format("dddd, MMMM DD")}
-            <GoDotFill size={7} />
-            {dayjs(eventData.start).format("h:mm")}
-            <span>-</span>
-            {eventData.end != null && dayjs(eventData.end).format("h:mm a")}
+            {eventData.designation}
+            {eventData.designation && <GoDotFill size={7} />}
+            {eventData.mobile}
           </h5>
         </div>
       </div>
+
+      <div className="modal-cells">
+        <div>
+          <FaRegClock size={18} />
+        </div>
+        <div className="cell">
+          <h5>
+            {dayjs(eventData.start).format("dddd, MMMM DD")}
+            {eventData.end != null && (
+              <>
+                <GoDotFill size={7} />
+                {dayjs(eventData.start).format("h:mm")}
+                <span>-</span>
+                {dayjs(eventData.end).format("h:mm a")}
+              </>
+            )}
+          </h5>
+        </div>
+      </div>
+
+      {/* company info */}
+      {eventData.company_name && (
+        <div className="modal-cells">
+          <div>
+            <FaRegBuilding size={18} />
+          </div>
+          <div className="cell">
+            <p>
+              {eventData.company_name}
+              {eventData.company_location && (
+                <span>, {eventData.company_location}</span>
+              )}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
